a630b1ce8ea008ee6a2d8d71d1400b81019c628f
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-08 01:24:40 +0530

    Error resolution : If internet is not available in mobile phone then how to proceed?

--- app/src/main/java/com/rishabhkumar/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/rishabhkumar/bookhub/fragment/DashboardFragment.kt
@@ -1,7 +1,10 @@
 package com.rishabhkumar.bookhub.fragment
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -79,97 +83,116 @@ class DashboardFragment : Fragment() {
 
 
         //to check whether app got connected with internet or not
-//        btnCheckInternet = view.findViewById(R.id.btnCheckInternet)
-//        btnCheckInternet.setOnClickListener {
-//            if(ConnectionManager().checkConnectivity(activity as Context)){
-//                //internet is available
-//                val dialog = AlertDialog.Builder(activity as Context)
-//                dialog.setTitle("Success")
-//                dialog.setMessage("Internet Connection found")
-//                dialog.setPositiveButton("Ok"){
-//                    text,listener ->
-//                    //do nothing
-//                }
-//                dialog.setNegativeButton("Cancel"){
-//                    text,listener->
-//                    //do nothing
-//                }
-//                dialog.create()
-//                dialog.show()
-//
-//            }else{
-//                //internet is not available
-//                val dialog = AlertDialog.Builder(activity as Context)
-//                dialog.setTitle("Failed")
-//                dialog.setMessage("Internet Connection not found")
-//                dialog.setPositiveButton("Ok"){
-//                        text,listener ->
-//                    //do nothing
-//                }
-//                dialog.setNegativeButton("Cancel"){
-//                        text,listener->
-//                    //do nothing
-//                }
-//                dialog.create()
-//                dialog.show()
-//            }
-//        }
+        btnCheckInternet = view.findViewById(R.id.btnCheckInternet)
+        btnCheckInternet.setOnClickListener {
+            if(ConnectionManager().checkConnectivity(activity as Context)){
+                //internet is available
+                val dialog = AlertDialog.Builder(activity as Context)
+                dialog.setTitle("Success")
+                dialog.setMessage("Internet Connection found")
+                dialog.setPositiveButton("Ok"){
+                    text,listener ->
+                    //do nothing
+                }
+                dialog.setNegativeButton("Cancel"){
+                    text,listener->
+                    //do nothing
+                }
+                dialog.create()
+                dialog.show()
+
+            }else{
+                //internet is not available
+                val dialog = AlertDialog.Builder(activity as Context)
+                dialog.setTitle("Failed")
+                dialog.setMessage("Internet Connection not found")
+                dialog.setPositiveButton("Ok"){
+                        text,listener ->
+                    //do nothing
+                }
+                dialog.setNegativeButton("Cancel"){
+                        text,listener->
+                    //do nothing
+                }
+                dialog.create()
+                dialog.show()
+            }
+        }
 
 
         val queue = Volley.newRequestQueue(activity as Context)
 
         val url = "http://13.235.250.119/v1/book/fetch_books/"
 
-        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url,null, Response.Listener{
-            //here we will handle the response
-            val sucsess = it.getBoolean("success")
-            if(sucsess){
-                val data = it.getJSONArray("data")
-                for(i in 0 until data.length()){
-                    val bookJsonObject = data.getJSONObject(i)
-                    val bookObject = Book(
-                        bookJsonObject.getString("book_id"),
-                        bookJsonObject.getString("name"),
-                        bookJsonObject.getString("author"),
-                        bookJsonObject.getString("rating"),
-                        bookJsonObject.getString("price"),
-                        bookJsonObject.getString("image")
+        if(ConnectionManager().checkConnectivity(activity as Context)){
+            val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url,null, Response.Listener{
+                //here we will handle the response
+                val sucsess = it.getBoolean("success")
+                if(sucsess){
+                    val data = it.getJSONArray("data")
+                    for(i in 0 until data.length()){
+                        val bookJsonObject = data.getJSONObject(i)
+                        val bookObject = Book(
+                            bookJsonObject.getString("book_id"),
+                            bookJsonObject.getString("name"),
+                            bookJsonObject.getString("author"),
+                            bookJsonObject.getString("rating"),
+                            bookJsonObject.getString("price"),
+                            bookJsonObject.getString("image")
                         )
-                    bookInfoList.add(bookObject)
-                    recyclerAdapter = DashboardRecyclerAdapter(activity as Context,bookInfoList)
+                        bookInfoList.add(bookObject)
+                        recyclerAdapter = DashboardRecyclerAdapter(activity as Context,bookInfoList)
 
 
-                    recyclerDashboard.adapter = recyclerAdapter
+                        recyclerDashboard.adapter = recyclerAdapter
 
-                    recyclerDashboard.layoutManager = layoutManager
+                        recyclerDashboard.layoutManager = layoutManager
 
 
-                    //function to make row separation between items
-                    recyclerDashboard.addItemDecoration(
-                        DividerItemDecoration(
-                            recyclerDashboard.context,
-                            (layoutManager as LinearLayoutManager).orientation
+                        //function to make row separation between items
+                        recyclerDashboard.addItemDecoration(
+                            DividerItemDecoration(
+                                recyclerDashboard.context,
+                                (layoutManager as LinearLayoutManager).orientation
+                            )
                         )
-                    )
+                    }
+                }else{
+                    Toast.makeText(activity as Context,"Some error occured!!!",Toast.LENGTH_SHORT).show()
                 }
-            }else{
-                Toast.makeText(activity as Context,"Some error occured!!!",Toast.LENGTH_SHORT).show()
-            }
-            // println("Response is $it")
+                // println("Response is $it")
 
-        },Response.ErrorListener {
-            //here we will handle the errors
+            },Response.ErrorListener {
+                //here we will handle the errors
                 println("Error is $it")
-        }){
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String,String>();
-                headers["Content-type"] = "application/json"
-                headers["token"] = "38574cd7e8eafc"
-                return headers
+            }){
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String,String>();
+                    headers["Content-type"] = "application/json"
+                    headers["token"] = "38574cd7e8eafc"
+                    return headers
+                }
             }
-        }
 
-        queue.add(jsonObjectRequest)
+            queue.add(jsonObjectRequest)
+
+        }else{
+            val dialog = AlertDialog.Builder(activity as Context)
+            dialog.setTitle("Failed")
+            dialog.setMessage("Internet Connection not found")
+            dialog.setPositiveButton("Open Settings"){
+                    text,listener ->
+                val settingIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(settingIntent)
+                activity?.finish()
+            }
+            dialog.setNegativeButton("Exit"){
+                    text,listener->
+                ActivityCompat.finishAffinity(activity as Activity)//to close app
+            }
+            dialog.create()
+            dialog.show()
+        }
 
         return view
     }
